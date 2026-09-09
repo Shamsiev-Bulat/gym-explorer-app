f52de7c2c82ae48f12e30d57094c8c93fa65baae
Shamsiev Bulat <87166092+Shamsiev-Bulat@users.noreply.github.com>
2026-09-09 10:00:32 +0300

    Create config.js
    add config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+    BASE_URL: 'https://raw.githubusercontent.com/hasaneyldrm/exercises-dataset/main/',
+    DATA_URL: 'https://raw.githubusercontent.com/hasaneyldrm/exercises-dataset/main/data/exercises.json',
+    DEFAULT_LANG: 'ru',
+    MAX_RENDER_ITEMS: 100,
+    STORAGE_KEYS: {
+        FAVORITES: 'favExercises',
+        LANGUAGE: 'appLanguage'
+    }
+};
